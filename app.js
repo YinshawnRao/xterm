63aaa853c115f38ba0f6df8e2b402107272e0145
YinshawnRao <raohu2008@qq.com>
2017-07-16 23:47:28 +0800

    ssh

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ var expressWs = require('express-ws')(app);
 
 //nodejs版的虚拟终端，需要python环境
 var pty = require('node-pty');
-//ssh
-var Client = require('ssh2').Client;
 
 var terminals = {},
     logs = {};
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -26,6 +25,12 @@ app.post('/terminals', function(req, res, next){
   var cols = parseInt(req.query.cols),
       rows = parseInt(req.query.rows);
 
+  var host = req.body.inputHost,
+  	  username = req.body.inputUsername,
+  	  password = req.body.inputPassword;
+
+  var isSSH = (typeof host != 'undefined') && (typeof username != 'undefined') && (typeof password != 'undefined');
+
   var term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
     name: 'xterm-color',
     cols: cols,
@@ -37,25 +42,43 @@ app.post('/terminals', function(req, res, next){
   console.log('Created terminal with PID: ' + term.pid);
 
   //自定义SSH
-  term.write('ssh root@115.159.48.163\r');
+  isSSH && term.write('ssh '+username+'@'+host+'\r');
 
   terminals[term.pid] = term;
 
+  var isLogin = false;
+
   logs[term.pid] = '';
   term.on('data', function(data) { 
-		if(data.toString() === "root@115.159.48.163's password: ") {
-			term.write('340dAC4b5db061DEV2ba$%#&7^a9dec7eb7bb6de\r');			
-		} 	
-		if(data.toString().indexOf('Last login:') >= 0) {
-	  		term.write('clear\r');
-
-	  		res.send(term.pid.toString());
-  			res.end();
-
-	  	}
-			
+  		if(isSSH) {
+  			if(data.toString() === username+"@"+host+"'s password: ") {
+				term.write(password+'\r');			
+			} 	
+			if(data.toString().indexOf('Last login:') >= 0) {
+		  		term.write('clear\r');
+		  		isLogin = true;
+		  		res.render('ssh', {
+		  			pid: term.pid.toString()
+		  		});
+		  		return;
+		  	}
+  		}	
    		logs[term.pid] += data;
-  });  
+  });
+
+  setTimeout(function() {
+  	if(isSSH && !isLogin) {
+		res.send('connect timeout');
+		res.end();
+		return;
+	}	
+  }, 3000);
+
+  if(!isSSH) {
+  	res.send(term.pid.toString());
+	res.end();
+	return;
+  }
 
 });
 
@@ -97,84 +120,6 @@ app.ws('/terminals/:pid', function (ws, req) {
 	    delete terminals[term.pid];
 	    delete logs[term.pid];
 	  });
-});
-
-
-var sshConn, sshLogs = '';
-
-//登录
-app.post('/ssh/login', function (req, res, next) {
-
-	sshLogs = '';
-	sshConn = new Client();
-	var body = req.body;
-
-	sshConn.connect({
-	  host: body.inputHost,
-	  port: 22,
-	  username: body.inputUsername,
-	  password: body.inputPassword
-	});
-
-	sshConn.on('ready', function() {
-		console.log('Client :: ready');
-
-		sshConn.shell(function(err, stream) {
-		    if (err) throw err;
-		    stream.on('close', function() {
-		      console.log('Stream :: close');		      
-		      sshConn.end();
-		    }).on('data', function(data) {
-
-		      sshLogs += data;
-
-		    }).stderr.on('data', function(data) {
-		      console.log('STDERR: ' + data);
-		    });
-		 });
-
-	});
-
-	res.render('ssh', {
-  		title: 'terminal ' + body.inputHost
-  	});
-
-});
-
-
-//登录
-//初始化信息
-
-
-app.ws('/ssh', function (ws, req) {
-
-	ws.send(sshLogs)
-// console.log(sshLogs)
-	ws.on('message', function(msg) {
-	   
-		sshConn.shell(function(err, stream) {
-		    if (err) throw err;
-		    stream.on('close', function() {
-		      console.log('Stream :: close');
-		      sshConn.end();
-		    }).on('data', function(data) {		    
-console.log(data.toString())
-		      ws.send(data.toString());
-
-		    }).stderr.on('data', function(data) {
-		      console.log('STDERR: ' + data);
-		    });
-
-		    stream.write(msg);
-
-		 });		
-
-	});
-
-	ws.on('close', function () {
-	    sshConn.end();	    
-	});
-
 });
 
 
